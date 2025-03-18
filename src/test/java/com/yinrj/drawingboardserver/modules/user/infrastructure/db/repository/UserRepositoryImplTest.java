466126383313627b0b389fcebecca0bf244a12e8
luo1o1o1o <luojia@situdata.com>
2025-03-18 14:07:56 +0800

    移除 UserRepositoryImplTest.java 中的所有代码，注释掉相关导入和类定义，准备进行重构

--- src/test/java/com/yinrj/drawingboardserver/modules/user/infrastructure/db/repository/UserRepositoryImplTest.java
+++ src/test/java/com/yinrj/drawingboardserver/modules/user/infrastructure/db/repository/UserRepositoryImplTest.java
@@ -1,35 +1,37 @@
-package com.yinrj.drawingboardserver.modules.user.infrastructure.db.repository;
+// package
+// com.yinrj.drawingboardserver.modules.user.infrastructure.db.repository;
 
-import com.yinrj.drawingboardserver.modules.user.domain.entity.UserEntity;
-import com.yinrj.drawingboardserver.modules.user.domain.repository.UserRepository;
-import jakarta.annotation.Resource;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import com.yinrj.drawingboardserver.modules.user.domain.entity.UserEntity;
+// import
+// com.yinrj.drawingboardserver.modules.user.domain.repository.UserRepository;
+// import jakarta.annotation.Resource;
+// import org.junit.jupiter.api.Assertions;
+// import org.junit.jupiter.api.Disabled;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-class UserRepositoryImplTest {
+// @SpringBootTest
+// class UserRepositoryImplTest {
 
-    @Resource
-    private UserRepository userRepository;
+// @Resource
+// private UserRepository userRepository;
 
-    @Test
-    @Disabled
-    void testInsert() {
-        UserEntity user = new UserEntity();
-        user.setUsername("1234");
-        user.setStudentId("23444488");
-        userRepository.insert(user);
-    }
+// @Test
+// @Disabled
+// void testInsert() {
+// UserEntity user = new UserEntity();
+// user.setUsername("1234");
+// user.setStudentId("23444488");
+// userRepository.insert(user);
+// }
 
-    @Test
-    @Disabled
-    void testSelect() {
-        UserEntity user = userRepository.select("1234", "23444488");
-        assertNotNull(user);
-    }
+// @Test
+// @Disabled
+// void testSelect() {
+// UserEntity user = userRepository.select("1234", "23444488");
+// assertNotNull(user);
+// }
 
-}
+// }
